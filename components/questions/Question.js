71a7d4e9309442c05e61b5cfb0e02e4ea9a15582
NGFuture <78330887+NGFuture@users.noreply.github.com>
2023-04-06 22:15:40 -0700

    adds multiselect

--- components/questions/Question.js
+++ components/questions/Question.js
@@ -1,31 +1,62 @@
 import { Text, View } from "react-native";
 import { RadioButton, Checkbox } from "react-native-paper";
 import { styles } from "../../styles";
+import { INPUT_TYPE_MULTIPLE_SELECT, INPUT_TYPE_SINGLE_ANSWER } from "../../config/quiz";
 
 const Question = ({ question, index, userChoice, setUserChoice }) => {
     const { text, options = [], type } = question;
+    let choice = userChoice;
+    if (type === INPUT_TYPE_MULTIPLE_SELECT) {
+        if (!choice) {
+            choice = [];
+            // choice.length = options.lenght;
+            // choice.fill(false);
+        }
+        // return (
+        //     <Text>here</Text>
+        // );
+    };
     return (
         <View>
-            <Text style={styles.header2}>{`${index+1}. ${text}`}</Text>
+            <Text style={styles.header2}>{`${index + 1}. ${text}`}</Text>
             <View>
-                <RadioButton.Group onValueChange={setUserChoice} value={userChoice} >
-                {options.map((item) => {
-                    return type === 'SINGLE_ANSWER' ?
-                            <RadioButton.Item
-                                label={item.text}
-                                value={item.id}
-                                key={item.id}
-                                position='leading'
-                            />
-                            :
-                            <Checkbox.Item
-                                label={item.text}
-                                value={item.id}
-                                key={item.id}
-                                position='leading'
-                            />
-                })}
-                </RadioButton.Group>
+                {type === INPUT_TYPE_SINGLE_ANSWER &&
+                    <RadioButton.Group onValueChange={setUserChoice} value={userChoice} >
+                        {options.map((item) => {
+                            return (
+                                <RadioButton.Item
+                                    label={item.text}
+                                    value={item.id}
+                                    key={item.id}
+                                    position='leading'
+                                />
+                            )
+                        })}
+                    </RadioButton.Group>
+                }
+                {type === INPUT_TYPE_MULTIPLE_SELECT &&
+                    <View >
+                        {options.map((item) => {
+                            return (
+                                <Checkbox.Item
+                                    label={item.text}
+                                    status={choice.includes(item.id) ? 'checked' : 'unchecked'}
+                                    key={item.id}
+                                    position='leading'
+                                    onPress={
+                                        () => {
+                                            if (choice.includes(item.id)) {
+                                                setUserChoice(choice.filter((id)=> id !== item.id));
+                                            } else {
+                                                setUserChoice([...choice, item.id]);
+                                            }
+                                        }
+                                    }
+                                />
+                        )})}
+                    </View>
+
+                }
             </View>
         </View>
     );
